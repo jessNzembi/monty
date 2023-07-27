25ea44185826e500e7954514fa24530ee723f279
Jess <jessgatura@gmail.com>
2023-07-27 13:20:34 +0300

    pop

--- pop.c
+++ pop.c
@@ -11,12 +11,15 @@ void _pop(stack_t **head, unsigned int count)
 {
 	stack_t *ptr;
 
-	ptr = *head;
-	if (!ptr)
+	if (!(*head))
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", count);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
+	ptr = *head;
 	*head = ptr->next;
 	if (ptr->next)
 		ptr->next->prev = NULL;
